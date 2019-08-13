b9dbb0861dc11c935b2b25ac43be3ddf88a4360e
Duncan Mann <duncan.s.mann@gmail.com>
2019-08-13 21:36:19 +0000

    Organized options object

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 let data1 = [35, 24, 68, 87, 47]; // Test data
 let element1 = $('body');        // Test element (body tag);
 let options1 = {                // Test options
+  
+  // CHART SIZE (pixels) \\
   width: 500,  // Automatically converts to pixels - leave this so we can use the value in equations later
   height: 300,
 
+  // GRID OPTIONS \\ 
   axisMax: 100, 
-  axisMin: 0, 
-
-  barSpacing: 15,
+  axisMin: 0,
+  
+  // BAR OPTIONS \\
+  barSpacing: 20,
   barColor: ['orange', 'violet', 'blue', 'red', 'green'],
 
+  // LABEL OPTIONS \\
   labelSize: 40,
   labelNames : ["Label1", "Label2", "Label3", "Label4", "Label5"],
 
+  // COLOUR OPTIONS \\
   backgroundColor: '#0c7ba3',
   labelColor: 'white',
+
+ // TITLE OPTIONS \\
 
   title: undefined,
   titleSize: undefined,
@@ -25,8 +33,9 @@ $(document).ready( function() {
 
       function barChart(data, options, element) {
  
-        let $chartArea = $('<div>');  //Created div to contain bar chart
+        
         let $labelArea = $('<div>'); // Created larger div to hold the chartArea div, which will also have labels for each axis, as well as the title
+        let $chartArea = $('<div>');  //Created div to contain bar chart
 
         $labelArea.prependTo(element).attr("id", "barChart");   //Adds this div to the top of the body tag
   $('#barChart').css({
@@ -47,7 +56,7 @@ $(document).ready( function() {
     "align-items" : "flex-end",                      
     "width" : options.width,               // Adds CSS class width from options object
     "height": options.height,             // Adds CSS class height from options object
-    "background-color": options.backgroundColor,  // Adds CSS class background-color from options object
+    "background-color": options.backgroundColor,
     "background-image": 'url(asfalt-light.png)'  
       });
 
